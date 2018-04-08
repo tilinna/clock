57f36028cee6ee36b427766d2ccb3aa91d154fba
Timo Linna <timo.linna@gmail.com>
2018-04-08 23:17:12 +0300

    Merge timectx subpackage with clock
    - Renamed WithClock() to Context() and Clock() to FromContext().
    - Removed extra Lock/Unlock from deadlineContext()

--- context.go
+++ context.go
@@ -5,30 +5,98 @@ import (
 	"time"
 )
 
-// DeadlineContext returns a copy of the parent context with the mocked
-// deadline adjusted to be no later than d.
+type clockKey struct{}
+
+// Context returns a copy of parent in which the Clock c is associated with.
 //
-// Unlike timectx.WithClock, this doesn't associate the Mock instance
-// with the returned context.
-func (m *Mock) DeadlineContext(parent context.Context, d time.Time) (context.Context, context.CancelFunc) {
-	m.Lock()
-	defer m.Unlock()
-	return m.deadlineContext(parent, d)
+// If the clock is an instance of *Mock, all subsequent uses of the
+// context in this package are automatically mocked, else the standard
+// realtime clock is used.
+func Context(parent context.Context, c Clock) context.Context {
+	return context.WithValue(parent, clockKey{}, c)
 }
 
-// TimeoutContext returns DeadlineContext(parent, m.Now().Add(timeout)).
+// FromContext returns the clock associated with the context, or Realtime().
+func FromContext(ctx context.Context) Clock {
+	if c, ok := ctx.Value(clockKey{}).(Clock); ok {
+		return c
+	}
+	return Realtime()
+}
+
+// WithDeadline returns a copy of the parent context with the deadline adjusted
+// to be no later than d.
 //
-// Unlike timectx.WithClock, this doesn't associate the Mock instance
-// with the returned context.
-func (m *Mock) TimeoutContext(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
-	m.Lock()
-	defer m.Unlock()
-	return m.deadlineContext(parent, m.now.Add(timeout))
+// If the FromContext(parent) returns a *Mock, it is used to mock the deadline,
+// else context.WithDeadline is called directly.
+func WithDeadline(parent context.Context, d time.Time) (context.Context, context.CancelFunc) {
+	if m, ok := FromContext(parent).(*Mock); ok {
+		m.Lock()
+		defer m.Unlock()
+		return m.deadlineContext(parent, d)
+	}
+	return context.WithDeadline(parent, d)
+}
+
+// WithTimeout returns WithDeadline(parent, Clock(parent).Now().Add(timeout)).
+//
+// If the FromContext(parent) returns a *Mock, it is used to mock the deadline,
+// else context.WithTimeout is called directly.
+func WithTimeout(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+	if m, ok := FromContext(parent).(*Mock); ok {
+		m.Lock()
+		defer m.Unlock()
+		return m.deadlineContext(parent, m.now.Add(timeout))
+	}
+	return context.WithTimeout(parent, timeout)
+}
+
+// After is a convenience wrapper for FromContext(ctx).After.
+func After(ctx context.Context, d time.Duration) <-chan time.Time {
+	return FromContext(ctx).After(d)
+}
+
+// AfterFunc is a convenience wrapper for FromContext(ctx).AfterFunc.
+func AfterFunc(ctx context.Context, d time.Duration, f func()) *Timer {
+	return FromContext(ctx).AfterFunc(d, f)
+}
+
+// NewTicker is a convenience wrapper for FromContext(ctx).NewTicker.
+func NewTicker(ctx context.Context, d time.Duration) *Ticker {
+	return FromContext(ctx).NewTicker(d)
+}
+
+// NewTimer is a convenience wrapper for FromContext(ctx).NewTimer.
+func NewTimer(ctx context.Context, d time.Duration) *Timer {
+	return FromContext(ctx).NewTimer(d)
+}
+
+// Now is a convenience wrapper for FromContext(ctx).Now.
+func Now(ctx context.Context) time.Time {
+	return FromContext(ctx).Now()
+}
+
+// Since is a convenience wrapper for FromContext(ctx).Since.
+func Since(ctx context.Context, t time.Time) time.Duration {
+	return FromContext(ctx).Since(t)
+}
+
+// Sleep is a convenience wrapper for FromContext(ctx).Sleep.
+func Sleep(ctx context.Context, d time.Duration) {
+	FromContext(ctx).Sleep(d)
+}
+
+// Tick is a convenience wrapper for FromContext(ctx).Tick.
+func Tick(ctx context.Context, d time.Duration) <-chan time.Time {
+	return FromContext(ctx).Tick(d)
+}
+
+// Until is a convenience wrapper for FromContext(ctx).Until.
+func Until(ctx context.Context, t time.Time) time.Duration {
+	return FromContext(ctx).Until(t)
 }
 
 func (m *Mock) deadlineContext(parent context.Context, deadline time.Time) (context.Context, context.CancelFunc) {
-	m.Lock()
-	defer m.Unlock()
 	cancelCtx, cancel := context.WithCancel(parent)
 	if pd, ok := parent.Deadline(); ok && !deadline.After(pd) {
 		return cancelCtx, cancel
